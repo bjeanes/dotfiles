4f0cee42fe4e94ec6f37b54f9f61e0738e9509fd
Bodaniel Jeanes <me@bjeanes.com>
2010-06-30 21:19:24 +1000

    Clean up and add a few shopt options for bash configuration to make it a little more like current ZSH behaviour

--- bash/config.sh
+++ bash/config.sh
@@ -5,11 +5,11 @@ bind "set completion-ignore-case on"
 bind "set bell-style none" # No bell, because it's damn annoying
 bind "set show-all-if-ambiguous On" # this allows you to automatically show completion without double tab-ing
 
-shopt -s checkwinsize
-shopt -s histappend
-shopt -s cdable_vars
-shopt -s globstar 2>/dev/null # Bash 4 and above only
-shopt -s nullglob
+
+shopt -s checkwinsize histappend cdable_vars extglob nullglob cdspell cmdhist hostcomplete
+
+# Bash 4 and above only
+shopt -s globstar autocd checkjobs 2>/dev/null 
 
 complete -C $DOT_FILES/bash/rake-completion.rb -o default rake}
 
