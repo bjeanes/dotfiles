92e4e439967262cffaa87a91f15842f0c8e6d9b1
Bodaniel Jeanes <me@bjeanes.com>
2010-06-30 14:51:24 +1000

    Start working on config to be just values I actually care about

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,26 +4,32 @@ bindkey "\e\e[C" forward-word
 bindkey "\e\e[D" backward-word
 
 # http://www.scannedinavian.com/~shae/shae.zshrc
+# These are options I've copied but yet to totally investigate if it's what I want
 setopt alwaystoend             # when complete from middle, move cursor
-setopt autocd                  # change to dirs without cd
-setopt autopushd               # automatically append dirs to the push/pop list
-setopt cdablevars              # avoid the need for an explicit $
 setopt completeinword          # not just at the end
 setopt correct                 # spelling correction
-setopt extendedglob            # weird & wacky pattern matching - yay zsh!
 setopt histverify              # when using ! cmds, confirm first
 setopt interactivecomments     # escape commands so i can use them later
 setopt listpacked              # compact completion lists
-setopt listtypes               # show types in completion
 setopt noautomenu              # don't cycle completions
-setopt nobeep                  # i hate beeps
-setopt nocheckjobs             # don't warn me about bg processes when exiting
-setopt nohup                   # and don't kill them, either
-setopt nopromptcr              # don't add \n which overwrites cmds with no \n
-setopt nullglob
-setopt printexitvalue          # alert me if something's failed
 setopt pushdignoredups         # and don't duplicate them
 setopt recexact                # recognise exact, ambiguous matches
+setopt nullglob
+
+# These are options that I definitely want
+setopt sharehistory
+setopt correct                 # spelling correction
+setopt rmstarwait              # if `rm *` wait 10 seconds before performing it!
+setopt notify                  # notify of BG job completion immediately
+setopt printexitvalue          # alert me if something's failed
+setopt autocd                  # change to dirs without cd
+setopt autopushd               # automatically append dirs to the push/pop list
+setopt cdablevars              # avoid the need for an explicit $
+setopt nobeep                  # i hate beeps
+setopt nohup                   # and don't kill BG jobs when shell exits
+setopt extendedglob            # awesome pattern matching (ala Dir.glob() in Ruby)
+setopt promptcr                # ensure a new line before prompt is drawn
+setopt listtypes               # show types in completion
 
 # jump to each element in a path with m-f m-b, same
 # for kill-word, etc.
