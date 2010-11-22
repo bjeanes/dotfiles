901aa478b02e12b3e00cca829fc62323e95ec72b
Bodaniel Jeanes <me@bjeanes.com>
2010-11-22 23:40:55 +1000

    Only make calls to `bind` if we are in interactive mode (stops errors)

--- bash/config.sh
+++ bash/config.sh
@@ -1,16 +1,19 @@
 [[ -f /etc/bash_completion ]] && source /etc/bash_completion
 
 # readline settings
-bind "set completion-ignore-case on" 
-bind "set bell-style none" # No bell, because it's damn annoying
-bind "set show-all-if-ambiguous On" # this allows you to automatically show completion without double tab-ing
-bind '"\e\e[C": forward-word'
-bind '"\e\e[D": backward-word'
+case "$-" in
+*i*)
+  bind "set completion-ignore-case on"
+  bind "set bell-style none" # No bell, because it's damn annoying
+  bind "set show-all-if-ambiguous On" # this allows you to automatically show completion without double tab-ing
+  bind '"\e\e[C": forward-word'
+  bind '"\e\e[D": backward-word'
+esac
 
 shopt -s checkwinsize histappend cdable_vars extglob nullglob cdspell cmdhist hostcomplete
 
 # Bash 4 and above only
-shopt -s globstar autocd checkjobs 2>/dev/null 
+shopt -s globstar autocd checkjobs 2>/dev/null
 
 complete -C $DOT_FILES/bash/rake-completion.rb -o default rake}
 
