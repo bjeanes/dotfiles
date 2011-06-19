0ca87f524acfbb4140fbee4c50043773b44ae53d
Bodaniel Jeanes <me@bjeanes.com>
2011-06-19 12:29:09 -0500

    Remove unused save-directory stuff

--- shell/common.sh
+++ shell/common.sh
@@ -5,7 +5,6 @@ source $DOT_FILES/$CURRENT_SHELL/config.*sh
 source $DOT_FILES/shell/exports.sh
 source $DOT_FILES/shell/aliases.sh
 source $DOT_FILES/shell/functions.sh
-source $DOT_FILES/shell/save-directory.sh
 source $DOT_FILES/shell/prompt.sh
 source $DOT_FILES/shell/git.sh
 
