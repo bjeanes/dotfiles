ec39822e3d1946db02b9d843dd25d74cde4768d8
Bodaniel Jeanes <me@bjeanes.com>
2011-10-23 19:05:35 -0500

    Refactor

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -2,10 +2,9 @@ export ZSH=$DOT_FILES/zsh/lib/oh-my-zsh
 export ZSH_THEME="risto"
 plugins=(rails git ruby osx brew)
 
-if [ -d "$ZSH" ]; then
-  source $ZSH/oh-my-zsh.sh
-else
+if ! [ -d "$ZSH" ]; then
   echo "Cloning Oh My Zsh..."
   /usr/bin/env git clone git://github.com/robbyrussell/oh-my-zsh.git "$ZSH"
-  source $ZSH/oh-my-zsh.sh
 fi
+
+source $ZSH/oh-my-zsh.sh
