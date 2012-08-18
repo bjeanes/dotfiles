2785427bb41b11aa5f9784f47777ceef12cd6973
Bo Jeanes <me@bjeanes.com>
2012-08-17 22:25:13 -0500

    Some more default leiningen plugins

--- languages/clojure/lein/profiles.clj
+++ languages/clojure/lein/profiles.clj
@@ -1,5 +1,9 @@
 {:user {:plugins [[lein-beanstalk "0.2.2"]
                   [lein-light "0.0.4"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-clojars "0.9.0"]
+                  [lein-tarsier "0.9.1"]
                   [lein-outdated "0.1.0"]
                   [lein-ring "0.7.1"]
-                  [lein-swank "1.4.0"]]}}
+                  [lein-swank "1.4.0"]]
+        :vimclojure-opts {:repl true}}
