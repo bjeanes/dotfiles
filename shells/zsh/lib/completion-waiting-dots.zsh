b6a3a935fa2d158a39513f98e6e91c66d54825cd
Bo Jeanes <me@bjeanes.com>
2013-03-09 23:39:27 -0600

    Show when ZSH is waiting for completion

---
+++ shells/zsh/lib/completion-waiting-dots.zsh
@@ -0,0 +1,8 @@
+# http://stackoverflow.com/a/844299
+expand-or-complete-with-dots() {
+    echo -n "\e[31m...\e[0m"
+    zle expand-or-complete
+    zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots
