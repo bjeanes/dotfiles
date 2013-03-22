ecea8cb317ea404f13951f5c8c18e464fc903865
Bo Jeanes <me@bjeanes.com>
2013-03-21 19:42:17 -0500

    Rails 3+ has dbconsole now, so this isn't needed

--- shells/common/ruby.sh
+++ shells/common/ruby.sh
@@ -2,19 +2,3 @@ export GEM_OPEN_EDITOR="$editor"
 export IRBRC="$HOME/.irbrc"
 export RBXOPT="-Xrbc.db=/tmp/rbx -X19"
 export JRUBY_OPTS="--1.9"
-
-function __database_yml {
-  if [[ -f config/database.yml ]]; then
-    ruby -ryaml -rerb -e "puts YAML::load(ERB.new(IO.read('config/database.yml')).result)['${RAILS_ENV:-development}']['$1']"
-  fi
-}
-
-export PSQL_EDITOR='vim +"set syntax=sql"'
-function psql
-{
-  if [[ "$(__database_yml adapter)" == 'postgresql' ]]; then
-    PGDATABASE="$(__database_yml database)" "$(which psql)" "$@"
-    return $?
-  fi
-  "$(/usr/bin/which psql)" "$@"
-}
