597b1feda9f2b390db97abfac9308166a1afe9ce
Bo Jeanes <me@bjeanes.com>
2013-11-11 20:24:58 -0800

    Submodule some zsh deps

--- shells/zsh/lib/fish.zsh
+++ shells/zsh/lib/fish.zsh
@@ -1,2 +1,14 @@
 source $SHELL_FILES/lib/syntax-highlighting/zsh-syntax-highlighting.zsh
 source $SHELL_FILES/lib/history-substring-search/zsh-history-substring-search.zsh
+
+# bind UP and DOWN arrow keys
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+# bind P and N for EMACS mode
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+
+# bind k and j for VI mode
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
