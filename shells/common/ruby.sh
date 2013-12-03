846e0b9c273b4a1e95270afcf17985e8d6ae6760
Bo Jeanes <me@bjeanes.com>
2013-12-03 10:47:16 -0800

    Don't set default ruby from shell start up

--- shells/common/ruby.sh
+++ shells/common/ruby.sh
@@ -8,7 +8,6 @@ which=`which -a which | tail -n1` # avoid builtin
 if [ -f "/usr/local/share/chruby/chruby.sh" ]; then
   source /usr/local/share/chruby/chruby.sh
   source /usr/local/share/chruby/auto.sh
-  chruby 2.0.0-p0
 
   function current_ruby() {
     if [ "x$RUBY_ROOT" != "x" ]; then
