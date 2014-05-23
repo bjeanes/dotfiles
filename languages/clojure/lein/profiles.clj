d80e16e348f6c6d5a2a92ec153db4698f633948a
Bo Jeanes <me@bjeanes.com>
2014-05-23 09:05:41 -0700

    Update lein profiles

--- languages/clojure/lein/profiles.clj
+++ languages/clojure/lein/profiles.clj
@@ -1,15 +1,17 @@
 {:user {:plugins [[lein-beanstalk "0.2.7"]
-                  [lein-light "0.0.27"]
+                  [lein-light "0.0.44"]
                   [lein-marginalia "0.7.1"]
+                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]
                   [lein-clojars "0.9.1"]
-                  [lein-outdated "1.0.1"]
+                  [lein-ancient  "0.5.5"]
                   [lein-depgraph "0.1.0"]
                   [lein-pprint "1.1.1"]
-                  [lein-ring "0.8.7"]]
-        :dependencies [[clj-stacktrace "0.2.5"]]
-        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                            'print-cause-trace)
-                           new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                           'pst)]
-                       (alter-var-root orig (constantly @new)))]
-        :vimclojure-opts {:repl true}}}
+                  [lein-ring "0.8.10"]]
+        :dependencies [[io.aviso/pretty "0.1.11"]
+                       [alembic "0.2.1"]]
+        :repl-options {:nrepl-middleware [io.aviso.nrepl/pretty-middleware]}
+        :injections [#_(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                              'print-cause-trace)
+                             new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                             'pst)]
+                         (alter-var-root orig (constantly @new)))]}}
