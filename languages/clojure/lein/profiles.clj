78403a268fdaf1d8f882c8d555ad7e750b448a81
Bo Jeanes <me@bjeanes.com>
2015-08-07 15:22:42 +1000

    lein stuff

--- languages/clojure/lein/profiles.clj
+++ languages/clojure/lein/profiles.clj
@@ -1,17 +1,18 @@
-{:user {:plugins [[lein-beanstalk "0.2.7"]
-                  [lein-light "0.0.44"]
-                  [lein-marginalia "0.7.1"]
-                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]
+{:user {:plugins [;[lein-beanstalk "0.2.7"]
+                  ;[lein-marginalia "0.7.1"]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "0.3.0-SNAPSHOT"]
                   [lein-clojars "0.9.1"]
-                  [lein-ancient  "0.5.5"]
+                  [lein-ancient  "0.6.7"]
                   [lein-depgraph "0.1.0"]
-                  [lein-pprint "1.1.1"]
-                  [lein-ring "0.8.10"]]
-        :dependencies [[io.aviso/pretty "0.1.11"]
-                       [alembic "0.2.1"]]
-        :repl-options {:nrepl-middleware [io.aviso.nrepl/pretty-middleware]}
-        :injections [#_(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                              'print-cause-trace)
-                             new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                             'pst)]
-                         (alter-var-root orig (constantly @new)))]}}
+                  [lein-pprint "1.1.2"]
+                  [lein-ring "0.9.6"]]
+        :dependencies [[io.aviso/pretty "0.1.18"]
+                       [alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.10"]
+                       [clj-stacktrace "0.2.8"]]
+        :repl-options {:skip-default-intro true}
+        :global-vars {*print-length* 103
+                      *print-level* 15}
+        :injections [(require 'io.aviso.repl)
+                     (io.aviso.repl/install-pretty-exceptions)]}}
