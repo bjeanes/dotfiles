390cd79187e5b73ca01ee5d422ac27b641999214
Bo Jeanes <me@bjeanes.com>
2019-08-29 10:02:43 +1000

    Set MIX_ENV for editor

--- shells/common/exports.sh
+++ shells/common/exports.sh
@@ -1,6 +1,15 @@
 which vim &>/dev/null && editor="$(which vim)"
 which nvim &>/dev/null && editor="$(which nvim)"
 
+# Many editor integrations (linters, language server, etc) mess up Phoenix auto
+# reloading by compiling changed files themselves (causing Phoenix to think
+# that the live version is up-to-date).
+#
+# Each tool has various work-arounds to make this work, but the simplest thing
+# (for editors started from the terminal) is to simply change the environment
+# everything runs under by default.
+editor="env MIX_ENV=editor $editor"
+
 export EDITOR="$editor -f"
 export VISUAL="$editor"
 
