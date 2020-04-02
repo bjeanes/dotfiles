36c193629247868054f1044404dc88354864f9ac
Bo Jeanes <me@bjeanes.com>
2020-04-02 14:44:29 +1100

    Preserve existing PATH
    In WSL2, the PATH is pre-filled with Windows' PATH contents (which are
    still executable within WSL Linux distros). For instance, `code .` to
    open VS Code works by finding `code` in the Windows filesystem, which is
    conveniently already specified in the PATH.

--- shells/path.sh
+++ shells/path.sh
@@ -1,3 +1,11 @@
+function __prepend_path {
+  if [ -z "$(eval echo \$$1)" ]; then
+    eval "$1=\"$2\""
+  else
+    eval "$1=\"$2:\$$1\""
+  fi
+}
+
 function __append_path {
   if [ -z "$(eval echo \$$1)" ]; then
     eval "$1=\"$2\""
@@ -6,17 +14,16 @@ function __append_path {
   fi
 }
 
-[ -d "$HOME/bin" ]          && __append_path "new_path" "$HOME/bin"
-[ -d "$HOME/.cargo/bin" ]   && __append_path "new_path" "$HOME/.cargo/bin"
-[ -d "$HOME/.emacs.d/bin" ] && __append_path "new_path" "$HOME/.emacs.d/bin"
-[ -d "$DOT_FILES/bin" ]     && __append_path "new_path" "$DOT_FILES/bin"
-[ -d '/usr/local/bin' ]     && __append_path "new_path" "/usr/local/bin"
-[ -d '/usr/local/sbin' ]    && __append_path "new_path" "/usr/local/sbin"
-[ -d '/opt/local/bin' ]     && __append_path "new_path" "/opt/local/bin"
-[ -d '/opt/local/sbin' ]    && __append_path "new_path" "/opt/local/sbin"
-[ -d '/usr/X11/bin' ]       && __append_path "new_path" "/usr/X11/bin"
-[ -d '/usr/bin' ]           && __append_path "new_path" "/usr/bin"
-[ -d '/usr/sbin' ]          && __append_path "new_path" "/usr/sbin"
-[ -d '/bin' ]               && __append_path "new_path" "/bin"
-[ -d '/sbin' ]              && __append_path "new_path" "/sbin"
-PATH="$new_path"
+[ -d "$HOME/bin" ]          && __prepend_path "PATH" "$HOME/bin"
+[ -d "$HOME/.cargo/bin" ]   && __prepend_path "PATH" "$HOME/.cargo/bin"
+[ -d "$HOME/.emacs.d/bin" ] && __prepend_path "PATH" "$HOME/.emacs.d/bin"
+[ -d "$DOT_FILES/bin" ]     && __prepend_path "PATH" "$DOT_FILES/bin"
+[ -d '/usr/local/bin' ]     && __append_path "PATH" "/usr/local/bin"
+[ -d '/usr/local/sbin' ]    && __append_path "PATH" "/usr/local/sbin"
+[ -d '/opt/local/bin' ]     && __append_path "PATH" "/opt/local/bin"
+[ -d '/opt/local/sbin' ]    && __append_path "PATH" "/opt/local/sbin"
+[ -d '/usr/X11/bin' ]       && __append_path "PATH" "/usr/X11/bin"
+[ -d '/usr/bin' ]           && __append_path "PATH" "/usr/bin"
+[ -d '/usr/sbin' ]          && __append_path "PATH" "/usr/sbin"
+[ -d '/bin' ]               && __append_path "PATH" "/bin"
+[ -d '/sbin' ]              && __append_path "PATH" "/sbin"
