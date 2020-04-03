0444b73bd5052e07c3a8ac243d7166dd7a85d864
Bo Jeanes <me@bjeanes.com>
2020-04-03 20:31:46 +1100

    Remove shell aliases I no longer use
    (esp `ss`, which conflicts with an actual command)

--- shells/common/aliases.sh
+++ shells/common/aliases.sh
@@ -1,20 +1,13 @@
 which hub &>/dev/null && alias git='hub'
 
-alias ll='ls -lah'
-alias l='ls'
-alias b='cd -'
 alias cdr='cd ./$(git rev-parse --show-cdup)'
 
 # useful command to find what you should be aliasing:
 alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
 
-# rails stuff
-alias sc="if [ -f script/console ]; then script/console; else script/rails console; fi"
-alias ss="if [ -f script/server ]; then script/server; else script/rails server; fi"
-alias gen='if [ -f script/generate ]; then script/generate; else script/rails generate; fi'
 alias webshare='python -c "import SimpleHTTPServer;SimpleHTTPServer.test()"'
 
-alias pubkey="cat $HOME/.ssh/*.pub"
+alias pubkeys="cat $HOME/.ssh/*.pub"
 alias colorslist="set | egrep 'COLOR_\w*'"  # lists all the colors
 
 alias jsonify='python -mjson.tool'
