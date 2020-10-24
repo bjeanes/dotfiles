0a357dac9bb7034b36b18c95e36079dae2de9d05
Bo Jeanes <me@bjeanes.com>
2020-10-25 08:44:04 +1100

    OK this is how I want Tmux to work
    I _thought_ this was how it was working before but apparently not ?

--- shells/tmux.sh
+++ shells/tmux.sh
@@ -3,8 +3,8 @@
 # If we aren't already in Tmux or emacs, set it up
 if command -v tmux &>/dev/null && [ "$SSH_CONNECTION" -a -z "$TMUX" -a -z "$INSIDE_EMACS" -a -z "$EMACS" -a -z "$VIM" -a -z "$VIMRUNTIME" ]; then
     if tty >/dev/null; then
-        if ! tmux has-session -t login 2>/dev/null; then
-            tmux attach-session -t login
+        if tmux has-session -t login 2>/dev/null; then
+            tmux new-session -t login \; new-window
         else
             tmux new-session -s login
         fi
