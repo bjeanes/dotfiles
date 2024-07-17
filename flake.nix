96c6c56ba3fd540d534726f9bb294a15d774ac43
Bo Jeanes <me@bjeanes.com>
2024-07-17 14:05:22 +1000

    Re-order config so attached comment makes more sense
    I don't think the `programs.bash.enable = true` is actually needed here,
    because /etc/bashrc _was_ being amended with the correct paths even when
    not present. Nonetheless, having them both written explicitly here is a
    comfort.

--- flake.nix
+++ flake.nix
@@ -142,8 +142,8 @@
                 # This needs to be here in addition to the home-manager configuration below in order to write /etc/zshenv to correctly configure ZSH. This is confusing, but...
                 # https://github.com/LnL7/nix-darwin/issues/1003
                 # https://github.com/LnL7/nix-darwin/issues/922#issuecomment-2041430035
-                programs.bash.enable = true;
                 programs.zsh.enable = true;
+                programs.bash.enable = true;
 
                 security.pam.enableSudoTouchIdAuth = true;
                 users.users.${myUsername}.home = "/Users/${myUsername}";
