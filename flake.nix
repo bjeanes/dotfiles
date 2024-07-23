15a4f1a9ab13e7be395636ff216e82582d559036
Bo Jeanes <me@bjeanes.com>
2024-07-23 12:53:40 +1000

    Tap the brews to install

--- flake.nix
+++ flake.nix
@@ -154,6 +154,11 @@
                 };
 
                 homebrew.enable = true;
+                homebrew.taps = [
+                  "exoscale/tap"
+                  "terrastruct/tap"
+                ];
+
                 homebrew.brews = [
                   "exoscale/tap/exoscale-cli"
                   "terrastruct/tap/tala" # proprietary layout engine for D2
