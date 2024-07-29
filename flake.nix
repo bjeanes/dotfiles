ecd7e1c947e87784dae2cc47c6c80e9d1a353697
Bo Jeanes <me@bjeanes.com>
2024-07-29 14:10:16 +1000

    Flag Tailscale app on macOS to show Taildrive settings

--- flake.nix
+++ flake.nix
@@ -215,6 +215,10 @@
                   # use expanded save panel by default
                   NSGlobalDomain.NSNavPanelExpandedStateForSaveMode = true;
                   NSGlobalDomain.NSNavPanelExpandedStateForSaveMode2 = true;
+
+                  CustomSystemPreferences = {
+                    "io.tailscale.ipn.macos"."FileSharingConfiguration" = "show";
+                  };
                 };
               };
             };
