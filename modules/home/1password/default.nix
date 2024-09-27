4cce7d9b6a822499592a5e4488986203fd859420
Bo Jeanes <me@bjeanes.com>
2024-09-27 13:46:00 +1000

    Re-configure 1P socket to work inside devcontainers
    As per https://archive.is/hwIUY

--- modules/home/1password/default.nix
+++ modules/home/1password/default.nix
@@ -1,12 +1,29 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, config, ... }:
+let
+  _1pSocket = "/Users/${config.snowfallorg.user.name}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+in
 {
   config = {
     programs.git = lib.optionalAttrs pkgs.stdenv.isDarwin
       {
-        extraConfig = {
-          "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+        # extraConfig = {
+        #   "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+        # };
+      };
+
+
+    programs.ssh = lib.optionalAttrs pkgs.stdenv.isDarwin {
+      matchBlocks = {
+        "*".extraOptions = {
+          "IdentityAgent" = lib.strings.escapeShellArg _1pSocket;
         };
       };
+    };
+
+    home.sessionVariables = lib.optionalAttrs pkgs.stdenv.isDarwin {
+
+      SSH_AUTH_SOCK = _1pSocket;
+    };
 
     programs._1password-shell-plugins = {
       enable = true;
