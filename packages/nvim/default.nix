fd69f13f3ae3c200318b9e331e4700ccfe1fe9cc
Bo Jeanes <me@bjeanes.com>
2024-11-30 23:31:16 +1100

    Set spell check language to Australia/GB English

--- packages/nvim/default.nix
+++ packages/nvim/default.nix
@@ -1,8 +1,8 @@
-{ lib
-, inputs
-, system
-, pkgs
-, ...
+{
+  lib,
+  inputs,
+  system,
+  ...
 }:
 let
   inherit (inputs) khanelivim;
@@ -10,6 +10,5 @@ in
 khanelivim.packages.${system}.default.extend {
   imports = lib.snowfall.fs.get-non-default-nix-files-recursive ./.;
 
-  # TODO: overrides:
-  # - https://github.com/khaneliman/khanelivim/blob/42dbca505535e8da3950658766fa9003da121de7/packages/khanelivim/options.nix#L63C5-L64C1
+  opts.spelllang = "en_gb,en_au";
 }
