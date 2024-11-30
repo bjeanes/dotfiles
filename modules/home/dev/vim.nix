451627481fa041d22099a8f7f75ddd5313a7bdfc
Bo Jeanes <me@bjeanes.com>
2024-11-30 23:15:43 +1100

    Re-configure nvim as default env editor and add vi/vim aliases

--- modules/home/dev/vim.nix
+++ modules/home/dev/vim.nix
@@ -1,5 +1,21 @@
-{ inputs, system, ... }: {
+{ inputs, system, lib, ... }:
+let
+  nvim = inputs.self.packages.${system}.nvim.extend {
+    viAlias = lib.mkForce true;
+    vimAlias = lib.mkForce true;
+  };
+in
+{
   # If I want to explose minimal versions of this to different systems, there is .extend.appstream
   # https://github.com/nix-community/nixvim/blob/05331006/docs/platforms/standalone.md#extending-an-existing-configuration
-  home.packages = [ inputs.self.packages.${system}.nvim ];
+  home.packages = [
+    nvim
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
+
+  home.shellAliases.vimdiff = "nvim -d";
 }
