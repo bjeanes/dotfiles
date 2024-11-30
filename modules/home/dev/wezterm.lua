86cf515cd8c5a59fc8ffd5615f8a64065192e415
Bo Jeanes <me@bjeanes.com>
2024-11-30 16:53:57 +1100

    Jump word with Option+arrow in WezTerm

--- modules/home/dev/wezterm.lua
+++ modules/home/dev/wezterm.lua
@@ -1,10 +1,32 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.font = wezterm.font_with_fallback({
 	"MesloLGMDZ Nerd Font Mono",
 })
 --TODO: Keep this here until this issue is resolved: https://github.com/wez/wezterm/issues/5990
 config.front_end = "WebGpu"
+
+
+config.keys = {
+  -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act.SendKey {
+      key = 'b',
+      mods = 'ALT',
+    },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act.SendKey {
+      key = 'f',
+      mods = 'ALT'
+    },
+  },
+}
 
 return config
