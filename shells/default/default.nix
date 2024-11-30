094e8615fc6d05b96ecb73ad088686cd27879e08
Bo Jeanes <me@bjeanes.com>
2024-11-30 19:34:07 +1100

    Add snowfall-flake to default devShell

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -7,6 +7,7 @@ pkgs.mkShell {
     python310Packages.mdformat
     shfmt
     inputs.nil.packages.${system}.nil
+    inputs.snowfall-flake.packages.${system}.flake
     inputs.nixos-generators.packages.${system}.nixos-generate
   ];
 }
