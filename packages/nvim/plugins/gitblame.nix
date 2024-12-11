a7c5b6053ecde979611e9cdd067eb7a2efed296c
Bo Jeanes <me@bjeanes.com>
2024-12-11 12:52:22 +1100

    nvim: setup git blame virtual text

---
+++ packages/nvim/plugins/gitblame.nix
@@ -0,0 +1,9 @@
+# Shows git blame information as virtual text
+#
+# https://github.com/f-person/git-blame.nvim
+{
+  plugins.gitblame = {
+    enable = true;
+    settings.virtual_text_column = 60;
+  };
+}
