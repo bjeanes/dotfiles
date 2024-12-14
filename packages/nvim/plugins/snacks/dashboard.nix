4c205433d51cb85ee2de34c2faced9be461a62d5
Bo Jeanes <me@bjeanes.com>
2024-12-14 12:26:02 +1100

    Make dashboard just single pane for now

--- packages/nvim/plugins/snacks/dashboard.nix
+++ packages/nvim/plugins/snacks/dashboard.nix
@@ -58,44 +58,31 @@
         { section = "header"; }
         {
           section = "keys";
-          gap = 1;
-          padding = 1;
+          title = "Keymaps";
+          indent = 2;
+          #gap = 1;
+          padding = 2;
         }
         {
-          pane = 2;
+          # pane = 2;
           icon = " ";
           title = "Recent Files";
           section = "recent_files";
           indent = 2;
-          padding = [
-            1
-            13
-          ];
+          padding = 2;
+          # padding = [
+          #   0
+          #   13
+          # ];
         }
         {
-          pane = 2;
+          # pane = 2;
           icon = " ";
           title = "Projects";
           section = "projects";
           indent = 2;
-          padding = 1;
+          padding = 2;
         }
-        # {
-        #   pane = 2;
-        #   icon = " ";
-        #   title = "Git Status";
-        #   section = "terminal";
-        #   enabled.__raw = ''
-        #     function()
-        #       return Snacks.git.get_root() ~= nil
-        #      end
-        #   '';
-        #   cmd = "git status --short --branch --renames";
-        #   height = 5;
-        #   padding = 1;
-        #   ttl = 5 * 60;
-        #   indent = 3;
-        # }
       ];
     };
   };
