206d780ee22dde26ea377369dc7e0cb98b4f76cb
Bo Jeanes <me@bjeanes.com>
2024-12-14 12:53:33 +1100

    Set up mini.pick to replace vim.ui.select when enabled

--- packages/nvim/plugins/mini/pick.nix
+++ packages/nvim/plugins/mini/pick.nix
@@ -1,24 +1,33 @@
-{ myLib, ... }:
+{ myLib, lib, config, ... }:
+let
+  enabled = (config.plugins.mini.enable && lib.hasAttr "pick" config.plugins.mini.modules);
+in
 {
   plugins.mini = {
     modules = {
       pick = { };
       extra = { };
     };
+
+    luaConfig.post = lib.mkIf enabled # lua
+      ''
+        vim.ui.select = require('mini.pick').ui_select
+      '';
   };
 
-  keymaps = myLib.modeKeys "n" {
-    "<Leader>/" = {
-      action = "<cmd>Pick grep_live<CR>";
-      options.desc = "Live grep";
-    };
-    "<C-Space>" = {
-      action = "<cmd>Pick files<CR>";
-      options.desc = "File picker";
-    };
-    "<Leader><Leader>" = {
-      action = "<cmd>Pick resume<CR>";
-      options.desc = "Resume last picker";
-    };
-  };
+  keymaps = lib.mkIf enabled
+    (myLib.modeKeys "n" {
+      "<Leader>/" = {
+        action = "<cmd>Pick grep_live<CR>";
+        options.desc = "Live grep";
+      };
+      "<C-Space>" = {
+        action = "<cmd>Pick files<CR>";
+        options.desc = "File picker";
+      };
+      "<Leader><Leader>" = {
+        action = "<cmd>Pick resume<CR>";
+        options.desc = "Resume last picker";
+      };
+    });
 }
