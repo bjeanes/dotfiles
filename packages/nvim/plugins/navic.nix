066dd493375babd6bbfc662d15ddd8412d8f658e
Bo Jeanes <me@bjeanes.com>
2024-12-14 16:31:27 +1100

    Add navic to show LSP-based breadcrumb

---
+++ packages/nvim/plugins/navic.nix
@@ -0,0 +1,12 @@
+{
+  # Shows the breadcrumb lsp node path in lualine
+  plugins.navic = {
+    enable = true;
+
+    settings = {
+      lsp = {
+        auto_attach = true;
+      };
+    };
+  };
+}
