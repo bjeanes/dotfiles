16d1fd786a4e8f7c6ae006ef92e5f1c17073a3b2
Bo Jeanes <me@bjeanes.com>
2024-12-22 15:27:33 +1100

    fix: origin url had a typo

--- modules/nixos/autodeploy/default.nix
+++ modules/nixos/autodeploy/default.nix
@@ -1,10 +1,12 @@
 {
   services.comin = {
     enable = true;
-    remotes = [{
-      name = "origin";
-      url = "https://githuib.com/bjeanes/dotfiles.git";
-      branches.main.name = "main";
-    }];
+    remotes = [
+      {
+        name = "origin";
+        url = "https://github.com/bjeanes/dotfiles.git";
+        branches.main.name = "main";
+      }
+    ];
   };
 }
