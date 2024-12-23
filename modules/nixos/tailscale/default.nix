0603a825526134ce064fb452395a152184745713
Bo Jeanes <me@bjeanes.com>
2024-12-23 15:16:04 +1100

    feat: openFirewall for Tailscale

--- modules/nixos/tailscale/default.nix
+++ modules/nixos/tailscale/default.nix
@@ -4,6 +4,7 @@
     enable = true;
     authKeyFile = config.age.secrets.tailscale-auth-server.path;
     authKeyParameters.ephemeral = false;
+    openFirewall = true;
     extraUpFlags = [
       "--advertise-tags=tag:home,tag:server"
     ];
