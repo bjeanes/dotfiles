8d21600505c4f4e85c130327b9e16f9864fcea55
Bo Jeanes <me@bjeanes.com>
2024-12-23 15:12:25 +1100

    refactor: Remove `?ephemeral=` from auth key secrets and into options
    This also fixes the name of the secret file, which got renamed but not
    committed.

--- modules/nixos/tailscale/default.nix
+++ modules/nixos/tailscale/default.nix
@@ -3,6 +3,7 @@
   services.tailscale = {
     enable = true;
     authKeyFile = config.age.secrets.tailscale-auth-server.path;
+    authKeyParameters.ephemeral = false;
     extraUpFlags = [
       "--advertise-tags=tag:home,tag:server"
     ];
