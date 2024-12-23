298e618aa77079213cb6ec79ba17a072ac985eef
Bo Jeanes <me@bjeanes.com>
2024-12-23 22:48:43 +1100

    fix: skip changing owner and group of backed up files
    We are backing up over NFS to the NAS but the exports on the NAS are
    squashing user perms anyway.

--- modules/nixos/homelab/arr.nix
+++ modules/nixos/homelab/arr.nix
@@ -162,7 +162,7 @@ let
                 };
                 script = ''
                   set -eu
-                  ${pkgs.rsync}/bin/rsync -avuP ${lib.escapeShellArg cfg.configDir}/* /nas/docker/media/${name}/
+                  ${pkgs.rsync}/bin/rsync -avuP --no-o --no-g ${lib.escapeShellArg cfg.configDir}/* /nas/docker/media/${name}/
                 '';
               };
             })
