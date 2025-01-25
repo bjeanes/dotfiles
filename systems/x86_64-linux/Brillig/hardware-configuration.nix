aa547417e1cbe893af5a90011ca183ec5dad0b6b
Bo Jeanes <me@bjeanes.com>
2025-01-25 19:40:50 +1100

    feat: re-provision Brillig using ext4
    This is because https://github.com/atuinsh/atuin/issues/952 has revealed
    some issues with ZFS and SQLite that make me realise I don't have the
    time to learn a new filesystem. I want to, but it's too far from my
    immediate goals to do now.

--- systems/x86_64-linux/Brillig/hardware-configuration.nix
+++ systems/x86_64-linux/Brillig/hardware-configuration.nix
@@ -1,59 +1,42 @@
-{ config, lib, modulesPath, ... }:
 {
-  imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  config,
+  lib,
+  modulesPath,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "uas" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "nvme"
+    "usbhid"
+    "uas"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  services.zfs.autoScrub.enable = true;
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXROOT";
+    fsType = "ext4";
+  };
 
-  fileSystems."/" =
-    {
-      device = "zpool/root";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
-
-  fileSystems."/nix" =
-    {
-      device = "zpool/nix";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
-
-  fileSystems."/var" =
-    {
-      device = "zpool/var";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
-
-  fileSystems."/home" =
-    {
-      device = "zpool/home";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-    };
-
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-id/nvme-Micron_2200S_NVMe_512GB__19502564DB40-part1";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
-
-  swapDevices =
-    [
-      {
-        device = "/dev/disk/by-id/nvme-Micron_2200S_NVMe_512GB__19502564DB40-part2";
-        randomEncryption = true;
-      }
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/NIXBOOT";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
     ];
+  };
+
+  swapDevices = [
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
