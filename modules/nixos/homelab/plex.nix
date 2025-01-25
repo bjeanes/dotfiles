0cc60b66b8df4dfca28b153aa4f40ae77ae1b431
Bo Jeanes <me@bjeanes.com>
2025-01-25 16:30:58 +1100

    refactor: switch to LSIO Plex image

--- modules/nixos/homelab/plex.nix
+++ modules/nixos/homelab/plex.nix
@@ -111,6 +111,7 @@ in
             [
               # Ensure config directory exists, owned by user
               "d ${e cfg.configDir}                   0775 ${cfg.user} ${cfg.group} - -"
+              "d ${e cfg.configDir}/${pmsPath}        0775 ${cfg.user} ${cfg.group} - -"
               "d ${e cfg.configDir}/${e logsPath}     0775 ${cfg.user} ${cfg.group} 4w -" # expire logs
               "d ${e transcodePath}                   0775 ${cfg.user} ${cfg.group} 2d -" # expire transcode artifacts
 
@@ -128,11 +129,10 @@ in
 
           virtualisation.oci-containers.containers = {
             ${svc} = {
-              image = "plexinc/pms-docker:plexpass";
+              image = "lscr.io/linuxserver/plex:latest";
 
               extraOptions = [
                 "--network=host"
-                "--privileged=true"
               ];
 
               # TODO: instead of interleaving mounts between local and NAS, have a single
@@ -142,18 +142,25 @@ in
                 [
                   "/etc/localtime:/etc/localtime:ro"
                   "/dev/dri:/dev/dri:z"
-
+                  "${secrets.plex-token.path}:/run/secrets/plex-claim"
                   "${transcodePath}:/transcode"
 
-                  "/mnt/nfs/nas/media:/data"
+                  "/mnt/nfs/nas/media:/data" # TV Shows, Movies, Music, etc
 
-                  # Mount the config from NAS first, so that big files like media thumbnails and previews are stored on the NAS
-                  "${nasConfigPath}:/config"
+                  # Base config is local, in particular /config/Library, which
+                  # LSIO's Plex checks owner. If it doesn't match it performs
+                  # a lengthly and permission-denied attempt to chown, which
+                  # we want to avoid.
+                  "${cfg.configDir}:/config"
 
-                  # Then, mount the database from a local path so that we aren't writing a SQLite DB over the network
+                  # A lot of large metadata files, thumbnails, etc, are stored
+                  # here, so let's put that straight on the NAS instead
+                  "${nasConfigPath}/${pmsPath}:/config/${pmsPath}"
+
+                  # We want the SQLite DB to be local though, but it's a subpath of pmsPath, so overlay DB location locally
                   "${cfg.configDir}/${databasePath}:/config/${databasePath}"
 
-                  # But make sure those database backups still end up on the NAS
+                  # But... make sure those database backups still end up on the NAS
                   "${nasConfigPath}/${databaseBackupPath}:/config/${databaseBackupPath}"
 
                   # Log files should be local
@@ -164,8 +171,24 @@ in
 
               environment = {
                 TZ = "Australia/Melbourne";
-                ALLOWED_NETWORKS = myLib.lanSubnet;
-                CHANGE_CONFIG_DIR_OWNERSHIP = "false";
+                VERSION = "latest";
+                FILE__PLEX_CLAIM = "/run/secrets/plex-claim";
+                DOCKER_MODS = "linuxserver/mods:universal-stdout-logs";
+                LOGS_TO_STDOUT = lib.concatMapStringsSep "|" (log: "/config/${logsPath}/${log}.log") [
+                  "Plex Crash Uploader"
+                  "Plex Media Server"
+                  "Plex Tuner Service"
+                  "PMS Plugin Logs/com.plexapp.agents.fanarttv"
+                  "PMS Plugin Logs/com.plexapp.agents.imdb"
+                  "PMS Plugin Logs/com.plexapp.agents.lastfm"
+                  "PMS Plugin Logs/com.plexapp.agents.localmedia"
+                  "PMS Plugin Logs/com.plexapp.agents.movieposterdb"
+                  "PMS Plugin Logs/com.plexapp.agents.none"
+                  "PMS Plugin Logs/com.plexapp.agents.plexthememusic"
+                  "PMS Plugin Logs/com.plexapp.agents.themoviedb"
+                  "PMS Plugin Logs/com.plexapp.agents.thetvdb"
+                  "PMS Plugin Logs/com.plexapp.system"
+                ];
               };
             };
           };
@@ -191,12 +214,8 @@ in
         }
 
         (setEnvFromCommandsForContainer svc {
-          PLEX_UID = "${pkgs.coreutils}/bin/id -u ${cfg.user}";
-          PLEX_GID = "${pkgs.getent}/bin/getent group homelab | cut -d: -f3";
-        })
-
-        (setEnvFromFilesForContainer svc {
-          PLEX_CLAIM = secrets.plex-token.path;
+          PUID = "${pkgs.coreutils}/bin/id -u ${cfg.user}";
+          PGID = "${pkgs.getent}/bin/getent group homelab | cut -d: -f3";
         })
 
         (lib.mkIf cfg.backupToNAS {
