245920093293a1d68d9709a737a4d72d77f100a7
Bo Jeanes <me@bjeanes.com>
2025-01-25 14:28:49 +1100

    fix: rekey secrets for new host

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,12 @@
 let
   borogrove = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDwjs401oUl5CYv0bijTQyHQgRFJuCGbcpgUzrYSlMak";
   brillig = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2b879SKcdqQ5Jb8R/PdNAhJTYY4zxHTRmAX2Y9Nv/b";
+  tumtum = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA9/LnXIRJW8TfdfNnCBuo5HfU+fIUxLHMex9dBycClU";
   bjeanes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJykg+5TulcwmeKFYSjZmnrL5/Fo4kWmOV1fAyt41Evh";
 
   users = [ bjeanes ];
   systems = [
+    tumtum
     borogrove
     brillig
   ];
