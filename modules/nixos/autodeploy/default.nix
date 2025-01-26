02a18f1292f9f4671b91b70801314e8fe5649e47
Bo Jeanes <me@bjeanes.com>
2025-01-26 14:48:12 +1100

    feat: serve Comin status page over TS

--- modules/nixos/autodeploy/default.nix
+++ modules/nixos/autodeploy/default.nix
@@ -1,12 +1,26 @@
+{ config, lib, ... }:
 {
-  services.comin = {
-    enable = true;
-    remotes = [
-      {
-        name = "origin";
-        url = "https://github.com/bjeanes/dotfiles.git";
-        branches.main.name = "main";
-      }
-    ];
-  };
+  imports = [
+    {
+      services.comin = {
+        enable = true;
+        remotes = [
+          {
+            name = "origin";
+            url = "https://github.com/bjeanes/dotfiles.git";
+            branches.main.name = "main";
+          }
+        ];
+      };
+    }
+
+    (lib.mkIf config.services.tailscale.enable {
+      system.activationScripts = {
+        tsServeComin.text = # sh
+          ''
+            ${config.services.tailscale.package}/bin/tailscale serve --bg --set-path "/comin" "http://localhost:4242/status"
+          '';
+      };
+    })
+  ];
 }
