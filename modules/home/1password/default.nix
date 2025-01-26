862e2caf96348b984fb2355988dd773490a7c43b
Bo Jeanes <me@bjeanes.com>
2025-01-26 11:36:06 +1100

    chore: remove commented out code

--- modules/home/1password/default.nix
+++ modules/home/1password/default.nix
@@ -1,17 +1,14 @@
-{ lib, pkgs, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 let
   _1pSocket = "/Users/${config.snowfallorg.user.name}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
 in
 {
   config = {
-    programs.git = lib.optionalAttrs pkgs.stdenv.isDarwin
-      {
-        # extraConfig = {
-        #   "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-        # };
-      };
-
-
     programs.ssh = lib.optionalAttrs pkgs.stdenv.isDarwin {
       matchBlocks = {
         "*".extraOptions = {
