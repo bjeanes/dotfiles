ccef9df13ed2cdca9be7f0b1792177d37182fa3e
Bo Jeanes <me@bjeanes.com>
2025-02-01 17:02:18 +1000

    feat(wezterm): vary colours according to system light/dark mode

--- modules/home/catppuccin/default.nix
+++ modules/home/catppuccin/default.nix
@@ -7,7 +7,22 @@
 
   programs.wezterm.extraConfig = # lua
     ''
-      config.color_scheme = "Catppuccin Mocha"
+      function get_appearance()
+        if wezterm.gui then
+          return wezterm.gui.get_appearance()
+        end
+        return 'Dark'
+      end
+
+      function scheme_for_appearance(appearance)
+        if appearance:find 'Dark' then
+          return 'Catppuccin Mocha'
+        else
+          return 'Catppuccin Latte'
+        end
+      end
+
+      config.color_scheme = scheme_for_appearance(get_appearance())
     '';
 
   programs.bat.config.theme = "Catppuccin Mocha";
