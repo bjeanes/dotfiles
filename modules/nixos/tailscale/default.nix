af926dcbbd3d6bc1eecf1bf9970c55d926c84cce
Bo Jeanes <me@bjeanes.com>
2026-03-31 11:53:28 +1100

    fix: podman tailscale containers boot looping after restart
    Workaround was always to reboot machine but that may have only worked
    due to a timing issue where those containers started before
    `/etc/resolv.conf` was rewritten by tailscale on the host machine.

--- modules/nixos/tailscale/default.nix
+++ modules/nixos/tailscale/default.nix
@@ -9,4 +9,11 @@
       "--advertise-tags=tag:home,tag:server"
     ];
   };
+
+  # Tailscale service overwrites /etc/resolved.conf which makes podman/docker
+  # containers later trying to run their own tailscale connections fail because
+  # they can't resolve the API server properly so get stuck in a boot loop.
+  #
+  # https://github.com/tailscale/tailscale/issues/4254
+  services.resolved.enable = true;
 }
