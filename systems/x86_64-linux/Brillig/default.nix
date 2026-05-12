5077868ee916f775071ab0e247891acd303b9712
Bo Jeanes <me@bjeanes.com>
2026-05-12 13:11:59 +1000

    fix: mess around with USB mounts into hass
    context is that multiple antennas use the same generic chipset, so
    sharing the devices by manufacturer/product IDs doesn't work. Instead
    I have to mount paths in a fairly brittle way. Ultimately, it feels like
    I need to move away from VM...

--- systems/x86_64-linux/Brillig/default.nix
+++ systems/x86_64-linux/Brillig/default.nix
@@ -49,17 +49,25 @@
       #     vendorId = "0x1cf1";
       #     productId = "0x0030";
       #   }
-      {
-        # Nabu Casa SkyConnect
-        vendorId = "0x10c4";
-        productId = "0xea60";
-      }
+      #{
+      #  # Nabu Casa SkyConnect
+      #  vendorId = "0x10c4";
+      #  productId = "0xea60";
+      #}
+      #{
+      #  # 2357:0604 TP-Link TP-Link UB500 Adapter
+      #  vendorId = "0x2357";
+      #  productId = "0x0604";
+      #}
     ];
     serialDevices = [
       # ConBee II is unresponsive when exposed as a USB device, so putting it
       # here exposes it as a TTY device at /dev/ttyS1, which Zigbee2MQTT is
       # happy with.
-      "/dev/serial/by-id/usb-dresden_elektronik_ingenieurtechnik_GmbH_ConBee_II_DE2449538-if00"
+
+      # "/dev/serial/by-id/usb-dresden_elektronik_ingenieurtechnik_GmbH_ConBee_II_DE2449538-if00"
+      "/dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_a2e19fcaa678f011a0a0a1e70ba521c7-if00-port0"
+      "/dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_bcdc5f621992ed11b4bdcbd13b20a988-if00-port0"
     ];
   };
 
