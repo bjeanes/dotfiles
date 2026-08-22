53eed60b9d8f9274393b1ef08c62c9d0b1c42d98
Bo Jeanes <me@bjeanes.com>
2026-08-22 15:56:16 +1000

    feat: increase linux-builder VM size on darwin

--- modules/darwin/nix/default.nix
+++ modules/darwin/nix/default.nix
@@ -19,6 +19,17 @@
         "aarch64-linux"
         "x86_64-linux"
       ];
+      maxJobs = 4;
+
+      # Guest defaults (1 core / 3G / 20G disk) are too small to build a whole
+      # NixOS closure; Borogrove's alone unpacks to ~12.5G.
+      config = {
+        virtualisation = lib.mkForce {
+          cores = 8;
+          memorySize = 12288;
+          diskSize = 40960;
+        };
+      };
     };
     settings.trusted-users = [ "@admin" ];
 
